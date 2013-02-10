223d6ad20ddefe604f14d6623d265a49bb0716ea
Jonas Hamm <contact@jonashamm.de>
2013-02-10 14:00:41 +0100

    second commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="de">
+ 
+<head>
+<meta charset="utf-8" />
+<title>Test</title>
+<link rel="stylesheet" href="general.css" media="all" />
+</head>
+ 
+<body>
+<?php echo "hallo"; ?>
+</body>
+</html>
